b889eded4af2cb36306ec98ea4ec144ee9beae4d
Parthiv88 <parthivgundubogula@gmail.com>
2024-10-01 19:58:22 +0530

    diff .java file

---
+++ Diff.java
@@ -0,0 +1,12 @@
+class Main {
+
+  public static void main(String[] args) {
+    
+    int first = 10;
+    int second = 20;
+
+    // add two numbers
+    int diff = first - second;
+    System.out.println(firstnumber + " - " + secondnumber + " = "  + diff);
+  }
+}
